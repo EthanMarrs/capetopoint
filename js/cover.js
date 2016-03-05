2cc9aff89890c2c30c8f6f4ab3dbfc7d15e5e1e4
Ethan Marrs <ethanmarrs@gmail.com>
2016-03-05 21:34:19 +0200

    Fixed cover js, cleaned up structure.

--- js/cover.js
+++ js/cover.js
@@ -1,20 +1,14 @@
 var count = 1;
 var max = 5;
 
-while(true) {
-
-  setTimeout(changeImage, 2000);
-  changeImage(count);
-
-  if(count > max) {
-    //count = 0;
-    break;
-  }
-  else {
-    count++;
-  }
+function changeBackground() {
+    if (count < max) {
+        count++;
+    }
+    else {
+        count = 1;
+    }
+    $("#cover").css("background-image", "url(../images/cover/" + count + ".jpg)");
 }
 
-function changeImage(num) {
-  $(".cover").css("background-image", "url(../images/cover/"+ num +".jpg)")
-}
+setInterval(changeBackground, 4000);
