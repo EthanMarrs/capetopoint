e61c748c4114c0145341da85fdaf21c33721769d
Ethan Marrs <ethanmarrs@gmail.com>
2016-08-09 00:41:11 +0200

    Added route 62 tour

--- js/tour.js
+++ js/tour.js
@@ -11,10 +11,3 @@ $(".tab-choice").on("click", function() {
     google.maps.event.trigger(map, 'resize');
     map.setCenter(new google.maps.LatLng(latLng[0], latLng[1]));
 });
-
-$("#dropdown-nav-tab").on("change", function(event){
-    $(".tab").addClass("hidden");
-    $("#" + $(this).find("option:selected").val().toLowerCase()).removeClass("hidden");
-    google.maps.event.trigger(map, 'resize');
-    map.setCenter(new google.maps.LatLng(latLng[0], latLng[1]));
-});
