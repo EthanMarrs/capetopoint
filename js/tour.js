57a2d367f0f6b323e21d28a6de39fef10f61a6cf
Ethan Marrs <ethanmarrs@gmail.com>
2016-08-10 23:35:00 +0200

    Adding back mobile menu functionality

--- js/tour.js
+++ js/tour.js
@@ -10,3 +10,10 @@ $(".tab-choice").on("click", function() {
     google.maps.event.trigger(map, 'resize');
     map.setCenter(new google.maps.LatLng(latLng[0], latLng[1]));
 });
+
+$("#dropdown-nav-tab").on("change", function(event){
+    $(".tab").addClass("hidden");
+    $("#" + $(this).find("option:selected").val().toLowerCase()).removeClass("hidden");
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(new google.maps.LatLng(latLng[0], latLng[1]));
+});
